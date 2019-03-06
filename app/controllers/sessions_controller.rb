ebecd4785d1c1f11c258df570b44c54798e85300
MichaelRomero <liikemike@aim.com>
2019-03-06 12:37:59 -0500

    added headers and fixed login path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
 
   ###### LOGIN ######
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.find(params[:id])
+    byebug
+    @user = User.find_by(params[:session][:username])
     byebug
     redirect_to user_path(@user)
   end
