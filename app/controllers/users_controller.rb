80b3fa827b817df3fde57259ae7f887de15622f4
ch2isk4kos <ch2isk4kos@gmail.com>
2019-03-05 20:03:02 -0500

    added routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,21 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def new
+        @user = User.new
+    end
 
+    def create
+        @user = User.create(user_params)
+        redirect_to user_path(@user)
+    end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit([:first_name, :last_name, :gamer_tag, :password_digest, :avatar_url, :location])
+    end
 end
 
 # strong params is only for 'create', 'update', or 'delete'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     has_many :matchups
     has_many :games, through: :matchups
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+
 end
