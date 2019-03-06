75262d21d47f5735b6cb071e0bf25aff7aedd1f0
ch2isk4kos <ch2isk4kos@gmail.com>
2019-03-05 23:13:09 -0500

    added user validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,9 +10,32 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(user_params)
+        # @user = User.new(user_params)
+
+
+        if @user.valid?
+            redirect_to user_path(@user)
+        else
+            # flash[:message] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
         redirect_to user_path(@user)
     end
 
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to root_path
+    end
 
     private
 
